317a1ecef9e915cf198fae5bca6f43166bd375de
User Jlucas <jlucas@ironwood.106.net>
2015-07-27 15:08:18 -0400

    Remove ace-jump mode config
    Don't know what it does and it was shadowing visual block mode.

--- user.el
+++ user.el
@@ -136,22 +136,23 @@
 ;;; ace-jump-mode
 ;;;
 
+;; Disabled because C-x SPC shadows visual block mode
 ; https://github.com/winterTTr/ace-jump-mode
-(autoload
-  'ace-jump-mode
-  "ace-jump-mode"
-  "Emacs quick move minor mode"
-  t)
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+;(autoload
+;  'ace-jump-mode
+;  "ace-jump-mode"
+;  "Emacs quick move minor mode"
+;  t)
+;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
-(autoload
-  'ace-jump-mode-pop-mark
-  "ace-jump-mode"
-  "Ace jump back"
-  t)
-(eval-after-load "ace-jump-mode"
-  '(ace-jump-mode-enable-mark-sync))
-(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+;(autoload
+;  'ace-jump-mode-pop-mark
+;  "ace-jump-mode"
+;  "Ace jump back"
+;  t)
+;(eval-after-load "ace-jump-mode"
+;  '(ace-jump-mode-enable-mark-sync))
+;(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
 
 ;;;
 ;;; expand-region
