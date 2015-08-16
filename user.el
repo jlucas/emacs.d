52cc515b8fd3d8da25374995b74f00104d632f8e
jlucas <jlucas@ash.106.net>
2015-08-16 17:23:45 -0400

    Minor reformatting

--- user.el
+++ user.el
@@ -168,19 +168,19 @@
 (defun slime-repl-font-lock-setup ()
   (setq font-lock-defaults
         '(slime-repl-font-lock-keywords
-         ;; From lisp-mode.el
-         nil nil (("+-*/.<>=!?$%_&~^:@" . "w")) nil
-         (font-lock-syntactic-face-function
-         . lisp-font-lock-syntactic-face-function))))
+          ;; From lisp-mode.el
+          nil nil (("+-*/.<>=!?$%_&~^:@" . "w")) nil
+          (font-lock-syntactic-face-function
+           . lisp-font-lock-syntactic-face-function))))
 (add-hook 'slime-repl-mode-hook 'slime-repl-font-lock-setup)
 (defadvice slime-repl-insert-prompt (after font-lock-face activate)
   (let ((inhibit-read-only t))
     (add-text-properties
      slime-repl-prompt-start-mark (point)
      '(font-lock-face
-      slime-repl-prompt-face
-      rear-nonsticky
-      (slime-repl-prompt read-only font-lock-face intangible)))))
+       slime-repl-prompt-face
+       rear-nonsticky
+       (slime-repl-prompt read-only font-lock-face intangible)))))
 
 ;;;
 ;;; dired
