7aff1e906d4ac580b745d889ac453fe576d750f0
jlucas <jlucas@ironwood.106.net>
2015-10-01 11:57:40 -0400

    Enable ace-jump-mode

--- user.el
+++ user.el
@@ -178,23 +178,22 @@
 ;;; ace-jump-mode
 ;;;
 
-;; Disabled because C-x SPC shadows visual block mode
 ; https://github.com/winterTTr/ace-jump-mode
-;(autoload
-;  'ace-jump-mode
-;  "ace-jump-mode"
-;  "Emacs quick move minor mode"
-;  t)
-;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(autoload
+ 'ace-jump-mode
+ "ace-jump-mode"
+ "Emacs quick move minor mode"
+ t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
-;(autoload
-;  'ace-jump-mode-pop-mark
-;  "ace-jump-mode"
-;  "Ace jump back"
-;  t)
-;(eval-after-load "ace-jump-mode"
-;  '(ace-jump-mode-enable-mark-sync))
-;(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+(autoload
+ 'ace-jump-mode-pop-mark
+ "ace-jump-mode"
+ "Ace jump back"
+ t)
+(eval-after-load "ace-jump-mode"
+ '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x C-n") 'ace-jump-mode-pop-mark)
 
 ;; window-margin
 ;; https://github.com/aculich/window-margin.el.git
