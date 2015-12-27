ce30fe928fb396ace0f28a40e4561f063de2319f
jlucas <jlucas@ash.106.net>
2015-12-27 02:36:26 -0500

    Add a bunch of theme packages from MELPA
    apropospriate-theme
    arjen-grey-theme
    bliss-theme
    borland-blue-theme
    boron-theme
    solarized-theme
    soothe-theme
    subatomic-theme
    subatomic256-theme

---
+++ elpa/apropospriate-theme-20151224.756/apropospriate-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "apropospriate-theme" "20151224.756" "A colorful, low-contrast, light & dark theme set for Emacs with a fun name." 'nil)
---
+++ elpa/solarized-theme-20151211.535/solarized-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "solarized-theme" "20151211.535" "The Solarized color theme, ported to Emacs." '((cl-lib "0.5") (dash "2.6.0")))
