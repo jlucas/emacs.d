a362e2b47a649eb46c482c100d35d5fdcf78f78d
jlucas <jlucas@localhost.localdomain>
2016-05-06 18:07:18 -0400

    Add commented code for setting frame font

--- user.d/theme.el
+++ user.d/theme.el
@@ -14,3 +14,10 @@
       (t
        (add-to-list 'default-frame-alist '(font . "Terminus-11"))))
 
+;; Set the font for the current frame
+;; (set-frame-font "Terminus-12" nil t)
+
+;; Set the font for future frames
+;; (set-face-attribute 'default nil :font "Terminus-12")
+
+
