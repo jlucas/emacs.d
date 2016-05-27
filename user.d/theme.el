980e774970d29b5b8b45773d894a47574e0f0542
jlucas <jlucas@ash.106.net>
2016-05-27 03:04:52 -0400

    Add some nice color themes, commented

--- user.d/theme.el
+++ user.d/theme.el
@@ -14,6 +14,46 @@
       (t
        (add-to-list 'default-frame-alist '(font . "Terminus-11"))))
 
+;;; Theme packages
+
+;; (use-package gotham-theme :ensure t)
+
+;; (use-package minimal-theme :ensure t)
+
+;; (use-package flatland-theme :ensure t)
+
+;; (use-package arjen-grey-theme :ensure t)
+
+;; (use-package gruvbox-theme :ensure t)
+
+;; (use-package darktooth-theme :ensure t)
+
+;; (use-package hemisu-theme :ensure t)
+
+;; (use-package hydandata-light-theme :ensure t)
+
+;; (use-package iodine-theme :ensure t)
+
+;; (use-package jazz-theme :ensure t)
+
+;; (use-package lavender-theme :ensure t)
+
+;; (use-package mbo70s-theme :ensure t)
+
+;; (use-package monochrome-theme :ensure t)
+
+;; (use-package mustang-theme :ensure t)
+
+;; (use-package mustard-theme :ensure t)
+
+;; (use-package planet-theme :ensure t)
+
+;; (use-package purple-haze-theme :ensure t)
+
+;; (use-package slime-theme :ensure t)
+
+;; https://github.com/owainlewis/emacs-color-themes
+
 ;; Set the font for the current frame
 ;; (set-frame-font "Terminus-12" nil t)
 
