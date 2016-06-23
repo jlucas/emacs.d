6a963b679ddc624d442e08976ee974ef950fb2cd
Jesse Lucas <jesse.lucas@framestore.com>
2016-06-23 02:51:32 -0400

    Remove package ibuffer-vc in favor of ibuffer-projectile

--- user.d/packages.el
+++ user.d/packages.el
@@ -111,14 +111,6 @@
   :ensure
   :config (wrap-region-global-mode t))
 
-(use-package ibuffer-vc
-  :ensure
-  :config (add-hook 'ibuffer-hook
-		      (lambda ()
-			(ibuffer-vc-set-filter-groups-by-vc-root)
-			(unless (eq ibuffer-sorting-mode 'alphabetic)
-			  (ibuffer-do-sort-by-alphabetic)))))
-
 (use-package openwith
   :ensure t
   :config (setq openwith-associations '(("\\.pdf" "xpdf" (file)))))
