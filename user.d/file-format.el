7fcb37585bfd23837b3ea868ddabd20e5f327f21
jlucas <jlucas@ash.106.net>
2016-08-14 10:52:10 -0400

    Remove old Python config

--- user.d/file-format.el
+++ user.d/file-format.el
@@ -35,31 +35,6 @@
      (setq indent-tabs-mode nil)))
 
 ;;
-;; python
-;;
-
-;; Perhaps look in to https://github.com/kaz-yos/eval-in-repl
-
-;; All moved to a use-package statement in packages.el
-
-;; (when (executable-find "ipython")
-;;   (setq python-shell-interpreter "ipython"))
-;; (defun ipython ()
-;;   (interactive)
-;;   (execute-extended-command 'run-python))
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args ""
-;;       python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-;;       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-;;       python-shell-completion-setup-code
-;;       "from IPython.core.completerlib import module_completion"
-;;       python-shell-completion-module-string-code
-;;       "';'.join(module_completion('''%s'''))\n"
-;;       python-shell-completion-string-code
-;;       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"
-;;       python-shell-completion-string-code
-;;       "';'.join(__IP.complete('''%s'''))\n"
-;;       python-shell-completion-module-string-code "")
 ;; strace
 ;;
 
