34b80ff5bf7248d2ecceb4e47b798baa7d2683f2
Jesse Lucas <jlucas@dorchester.ldn.framestore.com>
2016-08-24 05:04:24 +0100

    Load hs-minor-mode for specified major modes

--- user.d/file-format.el
+++ user.d/file-format.el
@@ -2,6 +2,18 @@
 ;;; File formats
 ;;;
 
+;;;
+;;; hide-show mode
+;;;
+
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
+(add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
+(add-hook 'java-mode-hook 'hs-minor-mode)
+(add-hook 'python-mode-hook 'hs-minor-mode)
+(add-hook 'lisp-mode-hook 'hs-minor-mode)
+(add-hook 'perl-mode-hook 'hs-minor-mode)
+(add-hook 'sh-mode-hook 'hs-minor-mode)
+
 ;;
 ;; tcl mode
 ;;
