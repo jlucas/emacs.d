1b7f086f934dee0b877ba5fbf37511d36fe5655e
jlucas <jlucas@ash.106.net>
2016-08-28 11:50:04 -0400

    Update evil config
    * Use kj/jk key-chord as <Esc> substitute
    * Add t to use-package :ensures
    * Remove unneeded progns from use-package :configs
    * Remove obsolete evil-jumper package
    * Update paredit.vim emulation maps

--- user.d/evil.el
+++ user.d/evil.el
@@ -2,69 +2,62 @@
 ;;; evil
 ;;;
 
-(message "Loading evil config...")
+(message "loading evil config...")
 
 (setq evil-toggle-key "C-M-z")
 
+(key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
+(key-chord-define evil-visual-state-map "kj" 'evil-normal-state)
+
 ;; Must be loaded before evil-mode
 (use-package evil-leader
-  :ensure
-  :init (progn
-	    (global-evil-leader-mode 1)
-	    (evil-leader/set-leader "<SPC>")
-	    (evil-leader/set-key
-	     "RET" (lambda ()
-	    	    (interactive)
-	    	    (dired (file-name-directory (buffer-file-name))))
-	     "e" 'find-file
-	     "f" 'fill-paragraph
-	     "w" 'save-buffer
-	     "W" 'paredit-wrap-round
-	     "c" 'delete-window
-	     "s" 'evil-window-split
-	     "S" 'paredit-splice-sexp
-	     "v" 'evil-window-vsplit
-	     "b" 'switch-to-buffer
-	     "B" 'ibuffer
-	     "n" 'linum-mode
-	     "q" 'save-buffers-kill-terminal
-	     "Q" 'kill-emacs
-	     "u" 'undo-tree-visualize
-	     "z" 'delete-other-windows
-	     ">" 'paredit-forward-slurp-sexp
-	     "<" 'paredit-forward-barf-sexp
-	     "}" 'paredit-backward-slurp-sexp
-	     "{" 'paredit-backward-barf-sexp)))
+  :ensure t
+  :config
+  (global-evil-leader-mode 1)
+  (evil-leader/set-leader "<SPC>")
+  (evil-leader/set-key
+    "RET" (lambda ()
+            (interactive)
+            (dired (file-name-directory (buffer-file-name))))
+    "e" 'find-file
+    "f" 'fill-paragraph
+    "w" 'save-buffer
+    "c" 'delete-window
+    "s" 'evil-window-split
+    "v" 'evil-window-vsplit
+    "b" 'switch-to-buffer
+    "B" 'ibuffer
+    "n" 'linum-mode
+    "q" 'save-buffers-kill-terminal
+    "Q" 'kill-emacs
+    "u" 'undo-tree-visualize
+    "z" 'delete-other-windows
+    ">" 'paredit-forward-slurp-sexp
+    "<" 'paredit-forward-barf-sexp
+    "}" 'paredit-backward-slurp-sexp
+    "{" 'paredit-backward-barf-sexp))
 
 (use-package evil
-  :ensure
+  :ensure t
   :init (evil-mode))
 
-(use-package evil-jumper
-  :ensure
-  :config (global-evil-jumper-mode))
-
 (use-package evil-visual-mark-mode
-  :ensure
+  :ensure t
   :config (evil-visual-mark-mode))
 
 (use-package evil-numbers
-  :ensure
-  :config (progn
-	    (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
-	    (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)))
+  :ensure t
+  :config
+  (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
+  (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt))
 
 (use-package evil-commentary
-  :ensure
+  :ensure t
   :config (evil-commentary-mode))
 
 (use-package evil-surround
-  :ensure
+  :ensure t
   :config (global-evil-surround-mode 1))
-
-;; (use-package evil-terminal-cursor-change
-;;   :ensure)
-
 
 ;; Make "C-]" do the right thing
 ;; https://emacs.stackexchange.com/questions/608/evil-map-keybindings-the-vim-way
@@ -93,6 +86,10 @@
   'paredit-forward-slurp-sexp)
 (define-key evil-normal-state-map (kbd ",<")
   'paredit-forward-barf-sexp)
+(define-key evil-normal-state-map (kbd "}")
+  'paredit-backward-slurp-sexp)
+(define-key evil-normal-state-map (kbd "{")
+  'paredit-backward-barf-sexp)
 (define-key evil-normal-state-map (kbd ",S")
   'paredit-splice-sexp)
 (define-key evil-normal-state-map (kbd ",W")
@@ -104,5 +101,3 @@
       (define-key slime-repl-mode-map (kbd "M-j") 'evil-window-down)
       (define-key slime-repl-mode-map (kbd "M-k") 'evil-window-up)
       (define-key slime-repl-mode-map (kbd "M-l") 'evil-window-right)))
-
-(message "Evil config loaded.")
