1187351cdbf1fa9f5ddf8636bb5c07744c329ec1
jlucas <jlucas@ash.106.net>
2016-09-03 10:47:41 -0400

    Always load evil

--- user.el
+++ user.el
@@ -10,12 +10,10 @@
 					  "~/.emacs.d/user.d/file-format.el"
 					  "~/.emacs.d/user.d/bind-user.el"
 					  "~/.emacs.d/user.d/bind-global-override.el"
+                      "~/.emacs.d/user.d/evil.el"
                       "~/.emacs.d/user.d/local.el")))
   (dolist (filepath config-files)
     (jl/load-if-readable filepath)))
-
-;; Use evil-mode
-;; (load-file "~/.emacs.d/user.d/evil.el")
 
 ;; Always load this file as an entry point into my config
 (find-file load-file-name)
