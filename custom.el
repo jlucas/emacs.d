11d583f1ad3058f6e7763f8058a7b501c286e395
jlucas <jlucas@ash.106.net>
2016-09-03 12:38:20 -0400

    Move Customize data into its own file

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("dcb157a776c26392399aadbfc05c50ca59db39a81f3d16d229227bb50b7b12b1" "9d7e517b49068e9fef941fe4083ad3d2a4b040895dca5175b84be48739689707" "d09467d742f713443c7699a546c0300db1a75fed347e09e3f178ab2f3aa2c617" "e56ee322c8907feab796a1fb808ceadaab5caba5494a50ee83a13091d5b1a10c" "1bdf0f4787a6bf53878ebc7c0ea322be27a412627e17e57e14a3a2e5210b6e98" "ba495a3cd1be02cfbb259f0b6e30c396a5511bf2acfdecf3541f4f2b91403a8c" "10b6f57c87df02b526df6fa5c1d92025148e98b3bd860f23de760c09685bdca6" "613f3b26da1310e615183d86a0568bc07b03e935b7210a09821181be9dc59d97" "a0f6c0774cb9ad3ac398f4ede0ffe8c0fb19887745783e9459d7c89327ee86a5" "0b2d512595d43d53dab6adf9fab9daa5e9d6d42c4c18c269354e0b8be96c50c1" default)))
+ '(elscreen-tab-display-kill-screen nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'dired-find-alternate-file 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
