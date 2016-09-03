a9ff32bfa26ac5918d63a4ca7997656993772707
jlucas <jlucas@ash.106.net>
2016-09-03 10:46:30 -0400

    Move sundry evil config into use-package form

--- user.d/evil.el
+++ user.d/evil.el
@@ -32,7 +32,66 @@
 
 (use-package evil
   :ensure t
-  :init (evil-mode 1))
+  :init
+  (evil-mode 1)
+  :config
+  ;; Leave evil waiting in the background
+  (setq evil-default-state 'emacs)
+
+  ;; Make "C-]" do the right thing
+  ;; https://emacs.stackexchange.com/questions/608/evil-map-keybindings-the-vim-way
+  (define-key evil-normal-state-map (kbd "C-]") (kbd "\\ M-."))
+
+  ;; expand-region binds matching vim-expand-region
+  (define-key evil-visual-state-map (kbd "+") 'er/expand-region)
+  (define-key evil-visual-state-map (kbd "-") 'er/contract-region)
+
+  ;; Move down a split and maximize it
+  (define-key evil-normal-state-map (kbd "C-j")
+    (lambda ()
+      (interactive)
+      (evil-window-down 1)
+      (evil-window-set-height (frame-height))))
+
+  ;; Poor man's paredit.vim
+  (define-key evil-normal-state-map (kbd ",>")
+    'paredit-forward-slurp-sexp)
+  (define-key evil-normal-state-map (kbd ",<")
+    'paredit-forward-barf-sexp)
+  (define-key evil-normal-state-map (kbd ",}")
+    'paredit-backward-slurp-sexp)
+  (define-key evil-normal-state-map (kbd ",{")
+    'paredit-backward-barf-sexp)
+  (define-key evil-normal-state-map (kbd ",S")
+    'paredit-splice-sexp)
+  (define-key evil-normal-state-map (kbd ",W")
+    'paredit-wrap-round)
+
+  (if (boundp 'evil-state)
+      (progn
+        (define-key slime-repl-mode-map (kbd "M-h") 'evil-window-left)
+        (define-key slime-repl-mode-map (kbd "M-j") 'evil-window-down)
+        (define-key slime-repl-mode-map (kbd "M-k") 'evil-window-up)
+        (define-key slime-repl-mode-map (kbd "M-l") 'evil-window-right)))
+
+  ;; Key chord instead of <Esc>  
+  (key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
+  (key-chord-define evil-visual-state-map "kj" 'evil-normal-state)
+
+  (if (equal evil-default-state 'emacs)
+      ;; Emacs
+      (loop for (mode . state) in '((dired-mode . emacs)
+                                    (package-menu-mode . emacs))
+            do (evil-set-initial-state mode state))
+    ;; Evil
+    (loop for (mode . state) in '((inferior-emacs-lisp-mode . emacs)
+                                  (ibuffer-mode . normal)
+                                  (dired-mode . normal)
+                                  (package-menu-mode . normal)
+                                  (org-mode . emacs)
+                                  (term-mode . emacs)
+                                  (shell-mode . emacs))
+          do (evil-set-initial-state mode state))))
 
 (use-package evil-visual-mark-mode
   :ensure t
@@ -65,60 +124,5 @@
   :ensure t
   :config (global-evil-surround-mode 1))
 
-;; Make "C-]" do the right thing
-;; https://emacs.stackexchange.com/questions/608/evil-map-keybindings-the-vim-way
-(define-key evil-normal-state-map (kbd "C-]") (kbd "\\ M-."))
 
-;; expand-region binds matching vim-expand-region
-(define-key evil-visual-state-map (kbd "+") 'er/expand-region)
-(define-key evil-visual-state-map (kbd "-") 'er/contract-region)
-
-;; Move down a split and maximize it
-(define-key evil-normal-state-map (kbd "C-j")
-  (lambda ()
-    (interactive)
-    (evil-window-down 1)
-    (evil-window-set-height (frame-height))))
-
-;; Move up a split and maximize it
-(define-key evil-normal-state-map (kbd "C-k")
-  (lambda ()
-    (interactive)
-    (evil-window-up 1)
-    (evil-window-set-height (frame-height))))
-
-;; Emulate paredit.vim
-(define-key evil-normal-state-map (kbd ",>")
-  'paredit-forward-slurp-sexp)
-(define-key evil-normal-state-map (kbd ",<")
-  'paredit-forward-barf-sexp)
-(define-key evil-normal-state-map (kbd ",}")
-  'paredit-backward-slurp-sexp)
-(define-key evil-normal-state-map (kbd ",{")
-  'paredit-backward-barf-sexp)
-(define-key evil-normal-state-map (kbd ",S")
-  'paredit-splice-sexp)
-(define-key evil-normal-state-map (kbd ",W")
-  'paredit-wrap-round)
-
-(if (boundp 'evil-state)
-    (progn
-      (define-key slime-repl-mode-map (kbd "M-h") 'evil-window-left)
-      (define-key slime-repl-mode-map (kbd "M-j") 'evil-window-down)
-      (define-key slime-repl-mode-map (kbd "M-k") 'evil-window-up)
-      (define-key slime-repl-mode-map (kbd "M-l") 'evil-window-right)))
-
-(setq evil-toggle-key "C-M-z")
-
-(key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
-(key-chord-define evil-visual-state-map "kj" 'evil-normal-state)
-
-(loop for (mode . state) in '((inferior-emacs-lisp-mode . emacs)
-                              (ibuffer-mode . normal)
-                              (dired-mode . normal)
-                              (package-menu-mode . normal)
-                              (org-mode . emacs)
-                              (term-mode . emacs)
-                              (shell-mode . emacs))
-      do (evil-set-initial-state mode state))
 
