9c98313bcda03b7f7511e274e119ea5709db7e7e
jlucas <jlucas@yew.106.net>
2017-08-26 10:42:47 -0400

    Move all terminal setup into terminal setup function

--- user.d/core.el
+++ user.d/core.el
@@ -155,13 +155,6 @@ http://stackoverflow.com/questions/11700934"
                          '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
 (global-set-key [f11] 'fullscreen)
 
-;;; Mouse scrolling and pointing in terminal mode
-(require 'xt-mouse)
-(defun up-slightly () (interactive) (scroll-up 2))
-(defun down-slightly () (interactive) (scroll-down 2))
-(global-set-key (kbd "<mouse-4>") 'down-slightly)
-(global-set-key (kbd "<mouse-5>") 'up-slightly)
-
 ;; Useful frame-related functions:
 ;;
 ;; framep (returns different values depending on frame attributes)
@@ -174,34 +167,40 @@ http://stackoverflow.com/questions/11700934"
 ;; https://www.emacswiki.org/emacs/MetaKeyProblems#toc16
 (defun jl/terminal-setup (&optional frame)
   (message "running terminal setup...")
+  (require 'xt-mouse)
   (xterm-mouse-mode t)
-  ;; From: http://offbytwo.com/2012/01/15/emacs-plus-paredit-under-terminal.html
-  ;; shift-arrow
-  (define-key input-decode-map "\e[1;2D" [S-left])
-  (define-key input-decode-map "\e[1;2C" [S-right])
-  (define-key input-decode-map "\e[1;2A" [S-up])
-  (define-key input-decode-map "\e[1;2B" [S-down])
-  ;; ctrl-arrow
-  (define-key input-decode-map "\e[1;5D" [C-left])
-  (define-key input-decode-map "\e[1;5C" [C-right])
-  (define-key input-decode-map "\e[1;5A" [C-up])
-  (define-key input-decode-map "\e[1;5B" [C-down])
-  ;; meta-arrow
-  (define-key input-decode-map "\e[1;3D" [M-left])
-  (define-key input-decode-map "\e[1;3C" [M-right])
-  (define-key input-decode-map "\e[1;3A" [M-up])
-  (define-key input-decode-map "\e[1;3B" [M-down])
-  ;; shift-meta-arrow
-  (define-key input-decode-map "\e[1;4D" [M-S-left])
-  (define-key input-decode-map "\e[1;4C" [M-S-right])
-  (define-key input-decode-map "\e[1;4A" [M-S-up])
-  (define-key input-decode-map "\e[1;4B" [M-S-down])
-  ;; ctrl-meta-arrow
-  (define-key input-decode-map "\e[1;7D" [C-M-left])
-  (define-key input-decode-map "\e[1;7C" [C-M-right])
-  (define-key input-decode-map "\e[1;7A" [C-M-up])
-  (define-key input-decode-map "\e[1;7B" [C-M-down])
-  (define-key input-decode-map "\e[13;5u" [(control return)]))
+  (defun up-slightly () (interactive) (scroll-up 2))
+  (defun down-slightly () (interactive) (scroll-down 2))
+  (global-set-key (kbd "<mouse-4>") 'down-slightly)
+  (global-set-key (kbd "<mouse-5>") 'up-slightly)
+  ;; ;; From: http://offbytwo.com/2012/01/15/emacs-plus-paredit-under-terminal.html
+  ;; ;; shift-arrow
+  ;; (define-key input-decode-map "\e[1;2D" [S-left])
+  ;; (define-key input-decode-map "\e[1;2C" [S-right])
+  ;; (define-key input-decode-map "\e[1;2A" [S-up])
+  ;; (define-key input-decode-map "\e[1;2B" [S-down])
+  ;; ;; ctrl-arrow
+  ;; (define-key input-decode-map "\e[1;5D" [C-left])
+  ;; (define-key input-decode-map "\e[1;5C" [C-right])
+  ;; (define-key input-decode-map "\e[1;5A" [C-up])
+  ;; (define-key input-decode-map "\e[1;5B" [C-down])
+  ;; ;; meta-arrow
+  ;; (define-key input-decode-map "\e[1;3D" [M-left])
+  ;; (define-key input-decode-map "\e[1;3C" [M-right])
+  ;; (define-key input-decode-map "\e[1;3A" [M-up])
+  ;; (define-key input-decode-map "\e[1;3B" [M-down])
+  ;; ;; shift-meta-arrow
+  ;; (define-key input-decode-map "\e[1;4D" [M-S-left])
+  ;; (define-key input-decode-map "\e[1;4C" [M-S-right])
+  ;; (define-key input-decode-map "\e[1;4A" [M-S-up])
+  ;; (define-key input-decode-map "\e[1;4B" [M-S-down])
+  ;; ;; ctrl-meta-arrow
+  ;; (define-key input-decode-map "\e[1;7D" [C-M-left])
+  ;; (define-key input-decode-map "\e[1;7C" [C-M-right])
+  ;; (define-key input-decode-map "\e[1;7A" [C-M-up])
+  ;; (define-key input-decode-map "\e[1;7B" [C-M-down])
+  ;; (define-key input-decode-map "\e[13;5u" [(control return)])
+  )
 (add-to-list 'tty-setup-hook 'jl/terminal-setup)
 
 (defun paste ()
