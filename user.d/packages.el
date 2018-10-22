9afd545edf63a7f0974daffd95611a038aaab589
jlucas <jlucas@yew.106.net>
2018-10-22 09:01:28 -0400

    Add RE Bulider config
    Invoke the re-builder with `M-x re-builder`.

--- user.d/packages.el
+++ user.d/packages.el
@@ -8,6 +8,28 @@
 (require 'diminish)
 (require 'bind-key)
 
+;; See: http://www.totherme.org/configs/gds.html
+;; See, also: https://www.masteringemacs.org/article/re-builder-interactive-regexp-builder
+(use-package re-builder
+  :config
+  (setq reb-re-syntax 'string)
+  (defun my/reb-query-replace-this-regxp (replace)
+    "Uses the regexp built with re-builder to query the target buffer.
+This function must be run from within the re-builder buffer, not the target
+buffer.
+
+Argument REPLACE String used to replace the matched strings in the buffer.
+ Subexpression references can be used (\1, \2, etc)."
+    (interactive "sReplace with: ")
+    (if (eq major-mode 'reb-mode)
+        (let ((reg (reb-read-regexp)))
+          (select-window reb-target-window)
+          (save-excursion
+            (beginning-of-buffer)
+            (query-replace-regexp reg replace)))
+      (message "Not in a re-builder buffer!")))
+  :bind (:map reb-mode-map ("C-c M-%" . my/reb-query-replace-this-regxp)))
+
 ;; atomic chrome
 ;; websockets server to launch an editor via web browser plugins like
 ;; GhostText and Atomic Chrome
